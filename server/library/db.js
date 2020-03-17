2f43b5acc156a9928e4d9748e334c7d09c24b317
ARSAN <arsandev@users.noreply.github.com>
2020-03-18 06:31:15 +0700

    change db concept

--- server/library/db.js
+++ server/library/db.js
@@ -7,27 +7,22 @@ class Database {
     this.table = table
   }
   query(q,cb){
-    if (typeof q === 'object') {
-      let key=[],val=[],crud=Object.keys(q)[0],cmd='',query=q[crud][0],funcQuery
-      if (crud === "select") funcQuery = "from"
-      else if (crud === "insert" || crud === "update") funcQuery = "into"
-      else if (crud === "delete") {
-        crud="remove"
-        funcQuery="from"
-      }
-      for (var keys in query) {
-        key.push(keys)
-        val.push(query[keys])
-      }
-      for (var i = 0; i < val.length; i++) {
-        cmd += key+"("+JSON.stringify(val[0])+")."
-      }
-      cmd = "sqlQuery."+crud+"()."+funcQuery+"('"+this.table+"')."+cmd+"build()"
-      let evalCmd = eval(cmd)
-      return db.query(""+evalCmd+"",(err,data)=>{
-        cb(err,data)
-      })
-    }
+    let query = q
+    db.query(query,(err,data)=>{
+      cb(err,data)
+    })
+  }
+  select(){
+    return sqlQuery.select().from(this.table)
+  }
+  update(){
+    return sqlQuery.update().into(this.table)
+  }
+  delete(){
+    return sqlQuery.remove().from(this.table)
+  }
+  insert(){
+    return sqlQuery.insert().into(this.table)
   }
 }
 
